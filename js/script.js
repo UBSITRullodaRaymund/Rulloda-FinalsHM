c045ac35bda4781382965557e9eec26c4f27b73e
UBSITRullodaRaymund <98291728+UBSITRullodaRaymund@users.noreply.github.com>
2022-05-15 18:43:27 +0800

    New Updates

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+    var stringVar = "This is my portfolio";
+    alert("hello " + stringVar)
+}
+
+alert("Hello this is My portfolio. \nYou can dismiss me by clicking in the ok button.")
