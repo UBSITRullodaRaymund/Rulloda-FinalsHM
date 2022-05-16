726824db728ba6a4f7974e211b02aa3e5d3cccef
UBSITRullodaRaymund <98291728+UBSITRullodaRaymund@users.noreply.github.com>
2022-05-15 20:39:36 -0500

    firebase deployment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-function showMessage(){
-    var stringVar = "This is my portfolio";
-    alert("hello " + stringVar)
-}
+
 
 alert("Hello and welcome to my portfolio. \nYou can ignore me by clicking the OK button..")
